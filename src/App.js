23019a413fa40edf3aae400ee651975a837dfb46
Robert Abreu <robertabreu@pursuit.org>
2019-02-15 21:42:51 -0500

    can get info from pokemon search in SearchBar up to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import pokemon from './pokemon'
-import SearchBar from './components/SearchBar';
-import Tags from './components/Tags';
+import NavBar from './components/NavBar';
+
 const pokemonNames = Object.keys(pokemon);
 
 
@@ -18,11 +18,20 @@ class App extends Component {
       masterList: pokemon,
     }
   }
+
+  testChange = (e) => {
+    if (e.keyCode === 13) {
+      console.log(e.target.value)
+    }
+    // console.log(e.target.value);
+
+  }
   // this.state.pokemon['charmander']; 
   render() {
     return (
       <>
         <h1>Hello World!</h1>
+        <NavBar pokemonList={pokemonNames} get={this.testChange}/>
       </>
     );
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,16 +2,17 @@ import React from 'react';
 import './SearchBar.css';
 
 const SearchBar = (props) => {
+
     return (
         <>
             <div className='row justify-content-md-center'>
                 <div className='nes-container with-title'>
-                    <h2 class="title">Search</h2>
+                    <h2 className="title">Search</h2>
                     <div className='col-md-auto'>
-                        <input className='nes-input' type="text" list="pokemonList" />
-                        <datalist className='nes-container is-rounded' id="pokemonList">
+                        <input className='nes-input' type="text" onKeyDown={props.get} list="pokemonList" />
+                        <datalist className='nes-container is-rounded'  id="pokemonList">
                             {props.results.map((e, i) => {
-                                return <option className='nes-container is-rounded' key={i}>{e}</option>
+                                return <option className='nes-container is-rounded' value={e} key={i}>{e}</option>
                             })}
                         </datalist>
                     </div>
