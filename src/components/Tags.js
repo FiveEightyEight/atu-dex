75e4e86393dc7d2923017ca6429181ed1902f98e
Robert Abreu <robertabreu@pursuit.org>
2019-02-17 22:22:46 -0500

    added layout to PokeProfile; modified tag.js, still needs css adjustment; Pokeprofile needs moves and movemodal

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -5,7 +5,7 @@ import './Tags.css'
 
 const Tags = (props) => {
 
-    let allClasses = `nes-btn bothTag ${props.type} disabled`;
+    let allClasses = `col-12 nes-btn bothTag ${props.type} disabled`;
 
     return (
         <button className={allClasses}>{props.name}</button>
