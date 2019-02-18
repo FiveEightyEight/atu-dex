362faba40baaf729d400205791792d7f3a89d2a6
Robert Abreu <robertabreu@pursuit.org>
2019-02-17 20:15:05 -0500

    PokeIndex components are a bit more dynamic

--- src/components/PokeIndex.js
+++ src/components/PokeIndex.js
@@ -6,9 +6,9 @@ const PokeIndex = (props) => {
     const altText = `${name} sprite`
     return (
         <div className='col-12 nes-container is-rounded row point' onClick={props.handleIndexClick} id={name.toLowerCase()}>
-            <img className='col-1' alt={altText} src={sprite} id={name.toLowerCase()} />
-            <h3 className='col-3' id={name.toLowerCase()} >{name}</h3>
-            <h3 className='ml-auto col-3' id={name.toLowerCase()}># {number}</h3>
+            <img className='col-sm-12 col-md-1' alt={altText} src={sprite} id={name.toLowerCase()} />
+            <h3 className='col-sm-12 col-md-6' id={name.toLowerCase()} >{name}</h3>
+            <h3 className='ml-auto col-sm-12 col-md-3' id={name.toLowerCase()}># {number}</h3>
         </div>
     )
 };
